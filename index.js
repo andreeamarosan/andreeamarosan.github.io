dfcbe78a672494807fd5f252e5d218b452d7f8aa
Andreea Marosan <andreeamarosan80@gmail.com>
2023-06-16 20:55:40 +0300

    use activePage to store last visible page

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
+var activePage = "home";
+
+// utilities functions
+
 function hide(id) {
   console.info("hide %o element", id);
   document.getElementById(id).style.display = "none";
 }
 
-function hideAllPages() {
-  hide("home");
-  hide("skills");
-  hide("languages");
-  hide("projects");
-}
-
 function showPage(id) {
-  hideAllPages();
+  // hideAllPages();
+  hide(activePage);
   var page = document.getElementById(id);
-  console.info("show", page);
+  console.info("show %o", page);
   page.style.display = "block";
+  activePage = id;
 }
 
-showPage("home");
+// start our code
+
+showPage(activePage);
